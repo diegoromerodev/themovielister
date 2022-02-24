245f3d76ecd66b96f8e42e5af1ab7a601b893ded
Diego Romero <diegoromeropersonal@gmail.com>
2022-02-24 19:55:28 -0300

    add get static props

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6f356f77ab42f80"; }
+/******/ 		__webpack_require__.h = function() { return "b1392df7ce8706a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-function HomePage() {
+function HomePage({ movie }) {
   return (
     <div>
       <h1>movielister</h1>
+      <h3>{movie.Title}</h3>
+      <img src={movie.Poster} alt={`${movie.Title} Poster`} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `http://www.omdbapi.com/?apikey=${process.env.MOVIEKEY}&s=avengers`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      movie: data.Search[0],
+    },
+  };
+};
 
 export default HomePage;
