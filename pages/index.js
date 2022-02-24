17ae3b0d36c6b584edddf5b6cbc914b34d5fbe1a
Diego Romero <diegoromeropersonal@gmail.com>
2022-02-23 22:14:18 -0300

    about demo page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-const HomePage = () => {
-    return <div>
-        <h1>movielister</h1>
+function HomePage() {
+  return (
+    <div>
+      <h1>movielister</h1>
     </div>
+  );
 }
 
 export default HomePage;
