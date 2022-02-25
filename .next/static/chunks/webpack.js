8c51e8cf750f05b4baadaf36e3bffae52917d6d1
Diego Romero <diegoromeropersonal@gmail.com>
2022-02-25 16:30:09 -0300

    Sass module

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1392df7ce8706a6"; }
+/******/ 		__webpack_require__.h = function() { return "9022e85a18ac92b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
-function HomePage({ movie }) {
+import useSWR from "swr";
+import styles from "../index.module.scss";
+
+function HomePage() {
+  const movieAPI = `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_MOVIEKEY}&s=avengers`;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data } = useSWR(movieAPI, fetcher);
+  if (!data) return <h1>Fetching data...</h1>;
+  const movie = data.Search[0];
   return (
     <div>
-      <h1>movielister</h1>
+      <h1 className={styles.mainHeader}>movielister</h1>
       <h3>{movie.Title}</h3>
       <img src={movie.Poster} alt={`${movie.Title} Poster`} />
     </div>
   );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.MOVIEKEY}&s=avengers`
-  );
-  const data = await res.json();
-  return {
-    props: {
-      movie: data.Search[0],
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   return {
+//     props: {
+//       movie: data.Search[0],
+//     },
+//     revalidate: 10,
+//   };
+// };
 
 export default HomePage;
