45b702c3db64d8a795ea5417be989ab346c78a18
Diego Romero <diegoromeropersonal@gmail.com>
2022-03-03 22:49:34 -0300

    Initial user schema

---
+++ database/user.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import pgSequelize from "../lib/sequelize";
+
+const User = pgSequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default User;
