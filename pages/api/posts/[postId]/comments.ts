c8a4946ba067148f014b04ce0c4b3fb3ec0d9573
Diego Romero <diegoromeropersonal@gmail.com>
2022-03-10 23:14:33 -0300

    initial posts routing

--- pages/api/posts/[postId]/comments.ts
+++ pages/api/posts/[postId]/comments.ts
@@ -1,14 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Comment from "../../../../schemas/comment";
 
-const postCommentsHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { postId } = req.query;
+const getPostComments = async (postId: number) => {
   const comments = Comment.findAll({
     where: {
       PostId: postId,
     },
   });
-  return res.json(comments);
+  return comments;
+};
+
+const createPostComment = () => {};
+
+const postCommentsHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const postId = Number(req.query.postId);
+  let commentData;
+  switch (req.method) {
+    default:
+      commentData = await getPostComments(postId);
+  }
+  return res.json(commentData);
 };
 
 export default postCommentsHandler;
