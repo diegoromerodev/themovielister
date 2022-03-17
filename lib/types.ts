558a0aac0fe9d4767286033ba3329553b4560265
Diego Romero <diegoromeropersonal@gmail.com>
2022-03-17 00:35:49 -0300

    post comments endpoints

--- lib/types.ts
+++ lib/types.ts
@@ -4,3 +4,12 @@ export interface PostSchema extends Model {
   setUser?: (user: Model) => void;
   UserId?: number | null;
 }
+
+export interface CommentSchema extends Model {
+  body?: string;
+}
+
+export interface CommentPostQuery {
+  postId?: string | number | undefined;
+  commentId?: string | number | undefined;
+}
--- pages/api/posts/[postId]/comments/[commentId].ts
+++ pages/api/posts/[postId]/comments/[commentId].ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { CommentPostQuery, CommentSchema } from "../../../../../lib/types";
 import Comment from "../../../../../schemas/comment";
 
 const getComment = async (postId: number, commentId: number) => {
@@ -11,10 +12,51 @@ const getComment = async (postId: number, commentId: number) => {
   return comment;
 };
 
+const deleteComment = async (postId: number, commentId: number) => {
+  const commentToDelete = await getComment(postId, commentId);
+  if (!commentToDelete) return false;
+  await commentToDelete.destroy();
+  return true;
+};
+
+const updateComment = async (
+  postId: number,
+  commentId: number,
+  body: string
+) => {
+  const commentToUpdate: CommentSchema = await getComment(postId, commentId);
+  if (!commentToUpdate) {
+    return false;
+  }
+  commentToUpdate.body = body;
+  const result = await commentToUpdate.save();
+  return result;
+};
+
 const commentsFinder = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { postId, commentId } = req.query;
-  const comment = await getComment(postId, commentId);
-  return comment;
+  let { postId, commentId }: CommentPostQuery = req.query;
+  const { body } = req.body;
+  postId = Number(postId);
+  commentId = Number(commentId);
+  let comment;
+  switch (req.method) {
+    case "DELETE":
+      comment = await deleteComment(postId, commentId);
+      break;
+    case "PUT":
+      if (body) {
+        comment = await updateComment(postId, commentId, String(body));
+      }
+      break;
+    default:
+      if (typeof commentId === "number" && typeof postId === "number") {
+        comment = await getComment(postId, commentId);
+      }
+  }
+  if (!comment) {
+    return res.status(404).json("COMMENT NOT FOUND");
+  }
+  return res.json(comment);
 };
 
 export default commentsFinder;
